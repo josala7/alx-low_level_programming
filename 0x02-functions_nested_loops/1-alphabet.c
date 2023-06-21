c8172a336727d66bec06fbd7ec21ef0121694c77
josala7 <joosala4@gmail.com>
2023-06-20 17:23:06 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include"main.h"
 
 /**
  * print_alphabet - utilizes on the _putchar function to print
@@ -9,7 +9,7 @@ void print_alphabet(void)
 {
 	int ch;
 
-	for (ch == 'a'; ch <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		_putchar(ch);
 	_putchar('\n');
 }
